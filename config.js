7b87e1e918b37da4c877a4e18b44369696a7d3ae
rag-hav <raghav.ag13@gmail.com>
2020-12-18 00:49:25 +0530

    nahi chal rha kuch bhi aur mein file add karna bhul gya tha

---
+++ config.js
@@ -0,0 +1,51 @@
+var def_configs = {
+        drive: {
+                active: {
+                        attribute_to_set: "checked",
+                        val: true,
+                },
+                default_account: {
+                        attribute_to_set: "value",
+                        val: 1,
+                },
+                restrict_to_gmail: {
+                        attribute_to_set: "checked",
+                        val: true,
+                },
+                url_regexs: [{
+                        regex: /(?<=https?:\/\/drive\.google\.com\/drive\/u\/)\d(?=.*)/,
+                        replace_prefix: "",
+                }, {
+                        regex: /(?<=https?:\/\/drive\.google\.com\/)drive(?=.*)/,
+                        replace_prefix: "drive/u/"
+                }]
+        },
+        classroom: {
+                active: {
+                        attribute_to_set: "checked",
+                        val: true,
+                },
+                default_account: {
+                        attribute_to_set: "value",
+                        val: 1,
+                },
+                restrict_to_gmail: {
+                        attribute_to_set: "checked",
+                        val: false,
+                },
+                url_regexs: [{
+                        regex: /(?<=https?:\/\/classroom\.google\.com\/u\/)\d(?=.*)/,
+                        replace_prefix: "",
+                }]
+        },
+}
+
+var openerRegexs = {
+        gmail: [/(?<=https?:\/\/mail\.google\.com\/mail\/u\/)\d(?=.*)/]
+}
+
+
+export {
+        def_configs,
+        openerRegexs
+};
